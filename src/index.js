ca5a9d29bcd4bb7cd079d76b967a5b440f8f3a90
lucas <lucas.klos@airesdigital.com.br>
2025-02-04 11:17:21 -0300

    feat: added firebaseAuthMiddleware

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export * from './discord/index.js';
 export * from './logs/index.js';
 export * from './utils/index.js';
 export * from './env/index.js';
+export * from './middlewares/index.js';
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,8 +3,9 @@ import {
     errorHandlerMiddleware
 } from './error-handler.middleware.js';
 
-
 export const errorHandler = {
     prepareRequest,
     errorHandlerMiddleware
 }
+
+export { createFirebaseAuthMiddleware } from './firebase-auth.middleware.js'
