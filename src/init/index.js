12963a97819e4320bcc767ccf1138b1baa541d4f
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-02-09 01:16:34 -0300

    feat: ajustes

--- src/init/index.js
+++ src/init/index.js
@@ -1,11 +1,14 @@
 import {globals} from "../globals/index.js";
+
 export const Init = {
     isInitialized: false,
-    inititalizeJsUtils(appName, admin, projectId) {
+    inititalizeJsUtils(appName, admin, projectId, clientsCollection) {
         if (!this.isInitialized) {
             globals.values.appName = appName;
             globals.values.projectId = projectId;
             globals.values.admin = admin;
+            globals.values.clientsCollection = clientsCollection;
+
             this.isInitialized = true;
         }
     },
