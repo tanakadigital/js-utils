7c586515d2c438b297560335ef41721534ea915f
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-02-19 20:57:29 -0300

    feat: add globals

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export * from './errors/index.js';
-export * from './discord/index.js';
-export * from './middlewares/index.js';
-export * from './utils/index.js';
-export * from './env/index.js';
-export * from './cloud-task/index.js';
-export * from './init/index.js';
-
-export * from './globals/index.js';
