2bd1a4ad49a29bdaebc9f2b581f7a1d640f2d785
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-02-26 09:50:45 -0300

    feat: ajustes

--- src/init/index.js
+++ src/init/index.js
@@ -1,19 +1,33 @@
 import {globals} from "../globals/index.js";
+import {BadRequestError} from "../errors/index.js";
 
 export const Init = {
     isInitialized: false,
-    inititalizeJsUtils(
-        appName, projectId,
-        discordWebhookUrl,
-        discordAppErrorsWebhookUrl
-    ) {
+    inititalizeJsUtils() {
         if (!this.isInitialized) {
 
-            globals.values.appName = appName;
-            globals.values.projectId = projectId;
+            if (!process.env.APP_NAME) {
+                throw new BadRequestError("App name not found");
+            }
 
-            globals.values.discordWebhookUrl = discordWebhookUrl;
-            globals.values.discordAppErrorsWebhookUrl = discordAppErrorsWebhookUrl;
+            if (!process.env.GCP_PROJECT_ID) {
+                throw new BadRequestError("GCP Project ID not found");
+            }
+
+            if (!process.env.DISCORD_DEFAULT_WEBHOOK_URL) {
+                throw new BadRequestError("Discord default webhook URL not found");
+            }
+
+            if (!process.env.DISCORD_APP_ERRORS_WEBHOOK_URL) {
+                throw new BadRequestError("Discord app errors webhook URL not found");
+            }
+
+            globals.values.appName = process.env.APP_NAME;
+            globals.values.projectId = process.env.GCP_PROJECT_ID;
+
+            globals.values.discordWebhookUrl = process.env.DISCORD_DEFAULT_WEBHOOK_URL;
+            globals.values.discordAppErrorsWebhookUrl = process.env.DISCORD_APP_ERRORS_WEBHOOK_URL;
+
 
             this.isInitialized = true;
 
