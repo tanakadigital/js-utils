56392a59b82066635b476d277899739951fcc210
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-03-03 15:37:31 -0300

    feat: add profiler module

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,5 +3,10 @@ export {
 } from "./prepare-request.middleware.js";
 
 export {
+    profilerMiddleware
+} from "./profiler.middleware.js"
+
+export {
     errorHandler,
 } from "./error-handler.middleware.js";
+
