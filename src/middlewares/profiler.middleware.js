849b3f971e281d35539c0fffc3b3457a7408d85d
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-03-03 15:34:33 -0300

    feat: add profiler module

---
+++ src/middlewares/profiler.middleware.js
@@ -0,0 +1,13 @@
+import {profiler} from "../profiler/profiler.js";
+
+export const profilerMiddleware = (req, res, next) => {
+    // Define o limite padrão para a requisição (pode ser alterado se necessário)
+    req.profiler = profiler.createProfiler(`${req.method} ${req.url}`, 1000);
+
+    // Quando a requisição terminar, finalizamos o profiler e verificamos se precisa notificar
+    res.on("finish", () => {
+        req.profiler.finish();
+    });
+
+    next();
+};
