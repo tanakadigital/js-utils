28065e5a86a3f137f4bc8ab0289b7e61ba46d670
ynogatz <ygor.nogatz@airesdigital.com.br>
2025-03-14 11:00:37 -0300

    fix: env-utils adjusts

--- src/middlewares/profiler.middleware.js
+++ src/middlewares/profiler.middleware.js
@@ -4,7 +4,7 @@ import {profiler} from "../profiler/profiler.js";
 export const profilerMiddleware = {
     middleware: (req, res, next) => {
         // Define o limite padrão para a requisição (pode ser alterado se necessário)
-        req.profiler = profiler.createProfiler(`${req.method} ${req.url}`, 1000);
+        req.profiler = profiler.createProfiler(`${req.method} ${req.url}`);
 
         // Quando a requisição terminar, finalizamos o profiler e verificamos se precisa notificar
         res.prependListener("finish", () => {
